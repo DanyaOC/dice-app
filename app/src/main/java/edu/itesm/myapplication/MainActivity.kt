8c2cbcf3b30b3a7829e7b558145ac3479a337363
DanyaOC <36273088+DanyaOC@users.noreply.github.com>
2021-02-19 19:26:29 -0600

    segundo commit app

--- app/src/main/java/edu/itesm/myapplication/MainActivity.kt
+++ app/src/main/java/edu/itesm/myapplication/MainActivity.kt
@@ -7,18 +7,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    var tiros = 0
+    var tiros = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("edu.itesm.daec", "demostración log")
+        mensaje.text ="Inicio"
+
         tira_dado()
     }
 
     fun tira_dado(){
-        roll_dice.setOnClickListener {
-            val rand = Random.nextInt(1, 6)
-            mensaje.text = "$rand tiro #${tiros++}"
-        }
+        var cont1 = 0
+        var cont2 = 0
+            roll_dice.setOnClickListener {
+                if (tiros <= 6) {
+                    var jugador: Int
+                    val rand = Random.nextInt(1, 7)
+                    if (tiros.rem(2) == 1) {
+                        jugador = 1
+                        cont1 += rand
+                    } else {
+                        jugador = 2
+                        cont2 += rand
+                    }
+                    mensaje.text = "Jugador: $jugador, Tiro #${tiros++}: $rand"
+                } else {
+                    if (cont1 > cont2){
+                        mensaje.text ="Gana el Jugador 1: $cont1"
+                    } else if (cont1 < cont2){
+                        mensaje.text ="Gana el Jugador 2: $cont2"
+                    } else {
+                        mensaje.text ="Empate"
+                    }
+                    tiros = 1
+                    cont1 = 0
+                    cont2 = 0
+                }
+            }
     }
 }
